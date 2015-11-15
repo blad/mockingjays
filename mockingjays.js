04383ba250012999ed2e45d6f3cca1b7c4b4a4ab
Bladymir Tellez <btellez@gmail.com>
2015-11-15 11:53:18 -0800

    Update Mockingjay to use clients, and record and repeat responses correctly.

--- mockingjays.js
+++ mockingjays.js
@@ -1,5 +1,22 @@
-exports = {
-  start: function(options) {
-    console.log('Starting with options', options);
-  }
+Server = require('./src/server');
+Mockingjay = require('./src/mockingjay');
+DefaultOptions = require('./src/default_options');
+
+var Mockingjays = function() {}
+
+/**
+ * Start creates a Mockingjays Server and starts proxying
+ * or mocking responses based on previous training.
+ *
+ * @param options - An {Object} with options
+ *          :cacheDir - Path to cache storage.
+ *          :port - Port that Mockingjays should bind to.
+ *          :serverBaseUrl - Base URL for the source server.
+ */
+Mockingjays.prototype.start = function(options) {
+  var finalOptions = DefaultOptions.merge(options);
+  var mockingjay = new Mockingjay();
+  Server.listen(finalOptions, function(req, res){ mockingjay.onRequest(req, res)});
 }
+
+module.exports = Mockingjays
