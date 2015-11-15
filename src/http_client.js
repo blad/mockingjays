2e4303fc7e7294d2cf8f5d992e507389eb864f46
Bladymir Tellez <btellez@gmail.com>
2015-11-15 10:03:17 -0800

    Add HttpClient to Make Requests

---
+++ src/http_client.js
@@ -0,0 +1,34 @@
+var url = require('url')
+var HttpClient = function() {}
+
+HttpClient.prototype.fetch = function (requestOptions) {
+  var self = this;
+  var urlSplit = url.parse(requestOptions.url);
+  var isHttps = urlSplit.protocol === 'https:'
+  var http = require(isHttps ? 'https' : 'http');
+  var options = {
+    hostname: urlSplit.hostname,
+    port: requestOptions.port || (isHttps ? 443 : 80),
+    path: urlSplit.path,
+    method: requestOptions.method,
+    headers: requestOptions.headers
+  };
+
+  return new Promise(function(resolve, reject) {
+    var req = http.request(options, function(res) {
+      var responseData = '';
+      res.on('data', function (chunk) {
+        responseData += chunk;
+      });
+      res.on('end', function() {
+        resolve(responseData);
+      });
+      res.on('error', function () {
+        reject('Unable to load data from request.');
+      });
+    });
+    req.end()
+  });
+}
+
+module.exports = HttpClient
