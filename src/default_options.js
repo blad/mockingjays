00b0e5745e0dcd51349d8b579f494ee4fe72606d
Bladymir Tellez <btellez@gmail.com>
2015-11-22 10:50:31 -0800

    Add Content-Type Blacklist Option
    - Refactor the DefaultOptions Handler to Have Helper Methods

--- src/default_options.js
+++ src/default_options.js
@@ -1,3 +1,4 @@
+var net = require('net');
 var FileSystemHelper = require('./filesystem_helper');
 
 /**
@@ -8,10 +9,33 @@ var DefaultOptions = function() {}
 DefaultOptions.prototype.options = {
   cacheDir: null,
   port: process.env.MOCKINGJAYS_PORT || 9000,
-  serverBaseUrl: null
+  serverBaseUrl: null,
+  ignoreContentType: ''
 }
 
 DefaultOptions.prototype.merge = function(options) {
+  this._handlePortDefault(options)
+  this._handleContentTypeDefault(options);
+  this._handleCacheDirectoryDefault(options);
+  this._handleBaseUrlDefault(options);
+  return options
+}
+
+DefaultOptions.prototype._handlePortDefault = function (options) {
+  var defaults = this.options;
+  options.port = options.port || defaults.port;
+}
+
+DefaultOptions.prototype._handleContentTypeDefault = function (options) {
+  var defaults = this.options;
+  var blacklist = (options.ignoreContentType || defaults.ignoreContentType)
+    .split(',');
+    .map(function (type) {return type.trim();});
+    .map(function (type) {return type.replace(/\*/g, '.*')});
+  options.ignoreContentType = blacklist;
+}
+
+DefaultOptions.prototype._handleCacheDirectoryDefault = function (options) {
   var defaults = this.options;
   // Directory where the cache files can be read and written to:
   options.cacheDir = options.cacheDir || defaults.cacheDir;
@@ -28,18 +52,16 @@ DefaultOptions.prototype.merge = function(options) {
         throw Error("Please Use a Writable Location for the Cache Directory.");
       });
   }
+}
 
-
-  // The PORT where the server should bind.
-  options.port = options.port || defaults.port;
+DefaultOptions.prototype._handleBaseUrlDefault = function (options) {
+  var defaults = this.options;
 
   // The base URL of the server to proxy requests to.
   options.serverBaseUrl = options.serverBaseUrl || defaults.serverBaseUrl;
   if (!options.serverBaseUrl) {
     throw Error("serverBaseUrl is required! It can not be empty.");
-  }
-
-  return options
+  };
 }
 
 module.exports = DefaultOptions
