08ea409d242fd7bd37b86f3bc4a85f7eee6ef3a4
Bladymir Tellez <btellez@gmail.com>
2018-12-10 08:56:09 -0800

    Fix Color Codes

--- src/colorize.js
+++ src/colorize.js
@@ -1,19 +1,19 @@
 let Colorize = {};
 
 Colorize.red = function(text) {
-  return "\\033[1;31m" + text + "\\033[0m";
+  return "\u{1b}[1;31m" + text + "\u{1b}[0m";
 }
 
 Colorize.green = function(text) {
-  return "\\033[1;32m" + text + "\\033[0m";
+  return "\u{1b}[1;32m" + text + "\u{1b}[0m";
 }
 
 Colorize.yellow = function(text) {
-  return "\\033[1;33m" + text + "\\033[0m";
+  return "\u{1b}[1;33m" + text + "\u{1b}[0m";
 }
 
 Colorize.blue = function(text) {
-  return "\\033[1;34m" + text + "\\033[0m";
+  return "\u{1b}[1;34m" + text + "\u{1b}[0m";
 }
 
 export default Colorize;
